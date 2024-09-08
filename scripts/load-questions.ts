b80d8c42cf3c3976c51e2fd1f1d964a8bf29294d
hwld <0701hw@gmail.com>
2024-09-08 10:18:25 +0900

    問題をgitで管理する

--- scripts/load-questions.ts
+++ scripts/load-questions.ts
@@ -1,26 +1,26 @@
-import { writeFile } from "fs/promises";
+import { mkdir, writeFile } from "node:fs/promises";
 import { getQuestionsMeta, type QuestionMeta } from "./get-questions-meta";
 import { getQuestionCode } from "./get-question-code";
+import { join } from "node:path";
 
 export type Question = QuestionMeta & { code: string };
 
+const questionsPath = "questions";
+
 const loadQuestions = async () => {
   const questionsMeta = await getQuestionsMeta();
-  const questions = (
-    await Promise.all(
-      questionsMeta.map(async (q): Promise<Question | null> => {
-        const code = await getQuestionCode(q.id);
-        if (!code) {
-          return null;
-        }
 
-        return { id: q.id, title: q.title, difficulty: q.difficulty, code };
-      })
-    )
-  ).filter((q) => q !== null);
+  await mkdir(questionsPath, { recursive: true });
 
   await Promise.all(
-    questions.map((q) => writeFile(`questions/${q.id}.json`, JSON.stringify(q)))
+    questionsMeta.map(async (q) => {
+      const code = await getQuestionCode(q.id);
+      if (!code) {
+        return;
+      }
+
+      await writeFile(join(questionsPath, `${q.id}.json`), JSON.stringify(q));
+    })
   );
 };
 
